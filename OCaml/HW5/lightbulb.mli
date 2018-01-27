70a3bfa8a0a51c9d1f339d6343d69612fe0c3b4a
Spencer Weiss <spweiss@seas.upenn.edu>
2018-01-27 15:10:24 -0500

    Delete lightbulb.mli

--- OCaml/HW5/lightbulb.mli
+++
@@ -1,6 +0,0 @@
-val mk_state_lightbulb : unit -> Widget.widget
-val mk_listener_lightbulb : unit -> Widget.widget
-val quit_w : Widget.widget
-val qnc : Widget.notifier_controller
-val qf : unit -> unit
-val w : Widget.widget
